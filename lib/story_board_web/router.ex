43b89a5220b2edfd963fbf18e7ed929fada2eb94
Oakley Reid <oakreid@gmail.com>
2019-04-07 21:24:18 -0400

    get routes

--- lib/story_board_web/router.ex
+++ lib/story_board_web/router.ex
@@ -17,6 +17,8 @@ defmodule StoryBoardWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/favorites", PageController, :index
+    get "/chat", PageController, :index
   end
 
   scope "/api", StoryBoardWeb do
